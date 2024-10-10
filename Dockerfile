712bd6d52127dfb95aeeb09104e0c975fd1a66d3
ducanhtranptit <tda.ducanh@gmail.com>
2024-10-10 09:59:59 +0700

    update code

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,13 @@
-# Use Node.js as base image
 FROM node:16
 
-# Set working directory
 WORKDIR /app
 
-# Copy package.json and package-lock.json
 COPY package*.json ./
 
-# Install dependencies
 RUN npm install
 
-# Copy the rest of the application
 COPY . .
 
-# Expose the port the app runs on
 EXPOSE 3000
 
-# Run database setup and start the app
-CMD npm run database && npm start
+CMD npm start
--- src/database/seeders/20240201035820-RoleSeeder.js
+++ src/database/seeders/20240201035820-RoleSeeder.js
@@ -2,21 +2,36 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-    async up(queryInterface, Sequelize) {
-        await queryInterface.bulkInsert(
-            "Roles",
-            [
-                {
-                    name: "Super Admin",
-                    createdAt: new Date(),
-                    updatedAt: new Date(),
-                },
-            ],
-            {}
-        );
-    },
+	async up(queryInterface, Sequelize) {
+		await queryInterface.bulkInsert(
+			"Roles",
+			[
+				{
+					name: "Super Admin",
+					createdAt: new Date(),
+					updatedAt: new Date(),
+				},
+				{
+					name: "Admin",
+					createdAt: new Date(),
+					updatedAt: new Date(),
+				},
+				{
+					name: "Giáo vụ",
+					createdAt: new Date(),
+					updatedAt: new Date(),
+				},
+				{
+					name: "Chăm sóc khách hàng",
+					createdAt: new Date(),
+					updatedAt: new Date(),
+				},
+			],
+			{}
+		);
+	},
 
-    async down(queryInterface, Sequelize) {
-        await queryInterface.bulkDelete("Roles", null, {});
-    },
+	async down(queryInterface, Sequelize) {
+		await queryInterface.bulkDelete("Roles", null, {});
+	},
 };
