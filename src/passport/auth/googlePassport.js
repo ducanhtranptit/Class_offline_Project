44979ea2a87262d8f48a974e7e475d4705a7f06d
ducanhtranptit <tda.ducanh@gmail.com>
2024-10-25 14:11:29 +0700

    fix bug google stategy

--- src/passport/auth/googlePassport.js
+++ src/passport/auth/googlePassport.js
@@ -4,38 +4,46 @@ const UserSocial = model.UserSocial;
 const User = model.User;
 
 module.exports = new GoogleStrategy(
-    {
-        clientID: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: process.env.GOOGLE_CALLBACK_URL,
-        passReqToCallback: true,
-        scope: ["profile", "email"],
-        prompt: "select_account",
-    },
-    async (req, accessToken, refreshToken, profile, done) => {
-        const { id } = profile;
+	{
+		clientID: process.env.GOOGLE_CLIENT_ID,
+		clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+		callbackURL: process.env.GOOGLE_CALLBACK_URL,
+		passReqToCallback: true,
+		scope: ["profile", "email"],
+		prompt: "select_account",
+	},
+	async (req, accessToken, refreshToken, profile, done) => {
+		const { id } = profile;
 
-        const provider = "google";
-        let providerDetail = await UserSocial.findOne({
-            where: {
-                provider: provider,
-                providerId: id,
-            },
-        });
-        if (!providerDetail?.userId) {
-            done(null, false, {
-                message: req.flash(
-                    "error",
-                    "Không tồn tại tài khoản nào liên kết với google này!"
-                ),
-            });
-            return;
-        }
-        const user = await User.findOne({
-            where: {
-                id: providerDetail.userId,
-            },
-        });
-        return done(null, user);
-    }
+		const provider = "google";
+		let providerDetail = await UserSocial.findOne({
+			where: {
+				provider: provider,
+				providerId: id,
+			},
+		});
+		if (!providerDetail?.userId) {
+			done(null, false, {
+				message: req.flash(
+					"error",
+					"Không tồn tại tài khoản nào liên kết với google này!"
+				),
+			});
+			return;
+		}
+		const user = await User.findOne({
+			where: {
+				id: providerDetail.userId,
+			},
+			attributes: [
+				"name",
+				"email",
+				"phone",
+				"address",
+				"typeId",
+				"firstLogin",
+			],
+		});
+		return done(null, user);
+	}
 );
